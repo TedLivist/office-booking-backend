23f509e295bcc0fc76e3b5f4ebcf1a42fc738e39
TedLivist <livistteddy@gmail.com>
2021-11-15 14:35:28 +0100

    Set up Item create action

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -9,4 +9,24 @@ class Api::V1::ItemsController < ApplicationController
     @item = Item.find(item_id)
     render json: @item
   end
+
+  def create
+    @item_to_save = Item.new(item_params)
+
+    if @item_to_save.save
+      render json: { success: 'Item saved' }
+    else
+      render json: { error: 'Unable to save item' }
+    end
+  end
+
+  def destroy
+    @item_to
+
+  private
+
+  def item_params
+    params.permit(:name, :location, :description)
+  end
+
 end

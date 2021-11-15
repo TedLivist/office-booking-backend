22a26feffafae87f48bcfc3ae5bee089a7a6d11f
unknown <safaaballagh97@gmail.com>
2021-11-15 11:49:59 +0100

    Set up reservations routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1, default: { format: 'json' } do
+    resources :reservations, only: [:index, :create, :destroy]
+  end
 end

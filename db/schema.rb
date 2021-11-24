43dbf7eb2205edd8d6c8f09d55aff34ad323ae9f
TedLivist <livistteddy@gmail.com>
2021-11-24 09:27:39 +0100

    Add uniqueness to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_093116) do
+ActiveRecord::Schema.define(version: 2021_11_24_081916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_093116) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "reservations", "items"
